6b7f32da4ed437cce354d14594e11eb7b28bb554
average <julianmuchacho666@gmail.com>
2023-03-19 17:16:53 -0600

    Initial commit

---
+++ Tests/Program.cs
@@ -0,0 +1,37 @@
+﻿using Drycodes.NET;
+
+namespace Tests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DrycodesApi api = new();
+
+            List<string> test = new List<string>()
+            {
+                api.GenerateBoyName(),
+                api.GenerateGirlName(),
+                api.GenerateMovie(),
+                api.GenerateObject(),
+                api.GeneratePlanet(),
+                api.GeneratePresident(),
+                api.GenerateRandomName(),
+                api.GenerateState(),
+                api.GenerateFunnyWord(),
+                api.GenerateGame(),
+                api.GenerateCityName(),
+                api.GenerateContinentName(),
+                api.GenerateCountryName(),
+                api.RandomPortNumber().ToString(),
+                api.RandomPrivilegedPortNumber().ToString()
+                
+            };
+            foreach(string t in test)
+            {
+                Console.WriteLine(t);
+            }
+            
+        }
+    }
+}
